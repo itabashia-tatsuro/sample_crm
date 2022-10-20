07707a5ff3aea1db5f44ac0a01f32d5937242b3e
itabashi-tatsuro <t.itabashi@a-cial.com>
2022-10-20 16:10:13 +0900

    fix item_factory

--- app/database/factories/ItemFactory.php
+++ app/database/factories/ItemFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Item::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->randomElement(['りんご', 'メロン', 'みかん', 'バナナ', 'ぶどう', 'キウイ']),
         'is_selling' => $faker->boolean,
         'price' => $faker->numberBetween(500, 10000),
         'memo' => $faker->realText(100),
